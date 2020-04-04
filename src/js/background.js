4e4d0fa4a374904213eb7ad6ba31620b62d3b5ba
adamkarminski <adamkarminski@gmail.com>
2020-04-04 13:31:54 +0200

    Naive implementation of clickable avatar

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,12 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+console.log('BJU - Im from backgroundaa')
+
+chrome.runtime.onMessage.addListener((message) => {
+	var messageObject = JSON.parse(message)
+
+	console.log('BJU - Halo')
+
+	sendJiraRequest(`issue/${messageObject.values.issueId}/assignee`)
+})
---
+++ src/js/content.js
@@ -0,0 +1,31 @@
+let init = () => {
+	// TODO: Load it when the UI is ready
+	let avatarHtml = '<img src="https://wnl-platform-production-storage.s3.eu-central-1.amazonaws.com/public/unassigned.png" class="ghx-avatar-img" data-tooltip="Unassigned">'
+
+	let issuesEndAreas = document.querySelectorAll('span.ghx-end')
+
+	console.log('BJU' + avatarHtml)
+
+	issuesEndAreas.forEach(e => {
+		if (e.innerHTML.indexOf('ghx-avatar-img') === -1) {
+			e.innerHTML = avatarHtml + e.innerHTML
+		}
+	})
+
+	document.querySelectorAll('img.ghx-avatar-img').forEach(element => {
+		if (element.getAttribute('listener') !== 'true') {
+			element.addEventListener('click', (e) => {
+				let parent = e.target.parentElement,
+					issueId = parent.getElementsByClassName('ghx-key')[0].innerText
+
+				console.log('BJU - Sending content message...')
+
+				chrome.runtime.sendMessage(`{"type":"assign","values":{"issueId":"${issueId}"}}`)
+			}, true)
+
+			element.setAttribute('listener', 'true')
+		}
+	})
+}
+
+window.onload = init;
